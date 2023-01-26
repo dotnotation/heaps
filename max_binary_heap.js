62ad5fb330095b792b8df9e35d621009ab808d4a
Dorthy Thielsen <dorthythielsen@gmail.com>
2023-01-26 15:28:27 -0500

    added base class, constructor, and insert function with notes

---
+++ max_binary_heap.js
@@ -0,0 +1,11 @@
+class MaxBinaryHeap{
+    constructor(){
+        this.values = []
+    }
+
+    insert(){
+        // push the value into the values property 
+        // bubble the value up to the correct spot
+        
+    }
+}
