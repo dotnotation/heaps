d7dabd56b2e4e904d586a67ee538e4c8fa668080
Dorthy Thielsen <dorthythielsen@gmail.com>
2023-01-26 15:59:47 -0500

    added pseudocode to insert function

--- max_binary_heap.js
+++ max_binary_heap.js
@@ -6,6 +6,11 @@ class MaxBinaryHeap{
     insert(){
         // push the value into the values property 
         // bubble the value up to the correct spot
-        
+            // create a variable called index which is the length of the values property - 1
+            // create a variable called parentIndex which is the floor of (index - 1)/2
+            // keep looping as long as the values element at the parentIndex is less than the values element at the child index
+                // swap the value of the values element at the parentIndex with the value of the element property at the child index
+                // set the index to be the parentIndex
+
     }
 }
