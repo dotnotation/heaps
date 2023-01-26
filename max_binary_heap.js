3244bb4d60c11e7f9767b61ccfc9916ec150d6a3
Dorthy Thielsen <dorthythielsen@gmail.com>
2023-01-26 17:43:42 -0500

    added seperate bubble up function, notes and code

--- max_binary_heap.js
+++ max_binary_heap.js
@@ -3,14 +3,31 @@ class MaxBinaryHeap{
         this.values = []
     }
 
-    insert(){
+    insert(element){
         // push the value into the values property 
+        this.values.push(element)
         // bubble the value up to the correct spot
-            // create a variable called index which is the length of the values property - 1
-            // create a variable called parentIndex which is the floor of (index - 1)/2
-            // keep looping as long as the values element at the parentIndex is less than the values element at the child index
-                // swap the value of the values element at the parentIndex with the value of the element property at the child index
-                // set the index to be the parentIndex
+        this.bubbleUp()
+    }
 
+    bubbleUp(){
+        // create a variable called index which is the length of the values property - 1
+        let idx = this.values.length - 1
+        const element = this.values[idx]
+        // keep looping as long as the values element at the parentIndex is less than the values element at the child index
+        while(true){
+            // create a variable called parentIndex which is the floor of (index - 1)/2
+            let parentIdx = Math.floor((idx - 1) / 2)
+            let parent = this.values[parentIdx]
+            if (element > parent){
+                // SWAP the value of the values element at the parentIndex with the value of the element property at the child index
+                this.values[parentIdx] = element
+                // set the index to be the parentIndex
+                this.values[idx] = parent
+            }
+        }
     }
 }
+
+let heap = new MaxBinaryHeap()
+heap.insert(55)
