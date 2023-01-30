12cfc8e108dab7ee7198c7f16a8ad44aacae9cee
Dorthy Thielsen <dorthythielsen@gmail.com>
2023-01-30 15:13:03 -0500

    added pseudocode and base functions

---
+++ priority_queue.js
@@ -0,0 +1,27 @@
+class Node{
+    constructor(){
+        this.val = val 
+        this.priority = 0
+    }
+}
+
+class PriorityQueue{
+    constructor(){
+        this.values = []
+    }
+
+    // lower priority number = highest priority 
+    // write a minBinaryHeap - lower number means higher priority
+
+    enqueue(val, priority){
+        // accepts a val and priority
+        // makes a new node
+        // put the new node in the right spot based on priority
+    }
+
+    dequeue(){
+        // removes root
+        // returns root
+        // rearrange heap using priority
+    }
+}
